43bab6047a8312e2de0ce8cfdceeab26be039c31
Juan Dionicio Jimenez Alcon <juan.jimenez@tde.com.bo>
2018-07-31 21:36:10 -0400

    mejoras de alertas

--- resources/views/Rol/listar_rol.blade.php
+++ resources/views/Rol/listar_rol.blade.php
@@ -32,7 +32,7 @@
                             <td> <a class="btn btn-primary" href="{{ route('editar_rol',$rol->id) }}">Editar</a> </td>
                             <td>
                                <!-- <a class="btn btn-danger" href="{{ route('eliminar_rol',$rol->id) }}">Eliminar</a> -->
-                            <button class="btn btn-danger" value="Eliminarr" onclick="eliminar()"> Eliminar</button>  </td>
+                            <button class="btn btn-danger" value="Eliminarr" onclick="eliminar({{$rol->id}}) , listar()"> Eliminar</button>  </td>
                           </tr>
                         @endforeach
                         </tbody>
@@ -46,15 +46,23 @@
 
     <link href="{{ asset('alertas/css/sweetalert.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('alertas/css/main.css') }}" rel="stylesheet">
-
+   
     <script src="{{ asset('alertas/js/sweetalert.min.js') }}" defer></script>
     <script src="{{ asset('alertas/js/functions.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
     <script type="text/javascript">
-        function eliminar(){
+
+
+    jQuery(document).ready(function(){
+       // $(location).attr('href', "{{ route('listar_rol') }}");
+       $a = 0;
+    }); 
+
+        function eliminar(id){
+   
             jQuery(document).ready(function(){
+
                 swal({   
                     title: "¿Seguro que deseas eliminar?",   
                     text: "No podrás deshacer este paso",   
@@ -66,16 +74,30 @@
                     closeOnConfirm: false }, 
 
                     function(){   
-
+                        
                         swal({   
                             title: "Eliminado correctamente",   
-                            text: "Se cerrará en 3 segundos.",   
+                            text: "Se cerrará en 1 segundos.",   
                             timer: 700,   
                             showConfirmButton: false 
                         });
+                        
+                        $(location).attr('href', "{{ route('eliminar_rol',$rol->id) }}");
+                        //$(location).attr('href', "{{ route('listar_rol') }}");
+                        //window.history.pushState(data, "Titulo", "{{ route('listar_rol') }}");
+                        //$("#enlace").click();
+                        //window.location = $('#enlace').attr('href');
+                        $a = 0;
                 });
             });  
+  
         }
+        function listar(){
+          
+           //window.history.pushState(data, "Titulo", "{{ route('listar_rol') }}");
+           //$(location).attr('href', "{{ route('listar_rol') }}")
+        }
+
     </script>
 
 @endsection
